0c90bed53b7758399b74910ecdea867f2d86c129
Kevin Pluck <kevinp@micad.co.uk>
2016-01-25 22:54:32 +0000

    Refactor

--- Vidja/Vidja.cs
+++ Vidja/Vidja.cs
@@ -8,52 +8,84 @@ namespace Vidja
 {
     public static class Vidja
     {
-        public static void Generate(IVidja vidja, string filename = "vidja.mp4", int startAt = 0)
+        private static Process _ffmpeg;
+        private static IVidja _vidja;
+        private static string _ffMpegPath;
+        private static int _startAt;
+        private static string _outputFilename;
+
+        public static void Generate(IVidja vidja, string outputFilename = "vidja.mp4", int startAt = 0)
         {
-            var ffMpegPath = ConfigurationManager.AppSettings["FFmpegExe"];
+            _outputFilename = outputFilename;
+            _startAt = startAt;
+            _vidja = vidja;
 
-            if (!File.Exists(ffMpegPath))
+            CheckForFfMpegExe();
+
+            StartFfMpegProcess();
+
+            PipeFramesToFfMpegProcess();
+        }
+
+        private static void CheckForFfMpegExe()
+        {
+            _ffMpegPath = ConfigurationManager.AppSettings["FFmpegExe"];
+
+            if (!File.Exists(_ffMpegPath))
             {
-                throw new InvalidOperationException("Vidja uses FFmpeg to generate video output.  If you have it installed please set the path to ffmpeg.exe in App.config otherwise please download FFmpeg from http://ffmpeg.org/");
+                throw new InvalidOperationException(
+                    "Vidja uses FFmpeg to generate video output.  If you have it installed please set the path to ffmpeg.exe in App.config otherwise please download FFmpeg from http://ffmpeg.org/");
             }
+        }
 
-            var ffmpeg = new Process
+        private static void StartFfMpegProcess()
+        {
+            _ffmpeg = new Process
             {
                 StartInfo = new ProcessStartInfo
                 {
-                    FileName = ConfigurationManager.AppSettings["FFmpegExe"],
-                    Arguments = $"-y -video_size {vidja.Width}x{vidja.Height} -r {vidja.Fps} -i pipe:0 -pix_fmt bgr24 {filename}",
+                    FileName = _ffMpegPath,
+                    Arguments =
+                        $"-y -video_size {_vidja.Width}x{_vidja.Height} -r {_vidja.Fps} -i pipe:0 -pix_fmt bgr24 {_outputFilename}",
                     UseShellExecute = false,
                     RedirectStandardInput = true,
                     WindowStyle = ProcessWindowStyle.Hidden
                 }
             };
 
-            ffmpeg.Start();
+            _ffmpeg.Start();
+        }
 
-            var input = ffmpeg.StandardInput.BaseStream;
+        private static void PipeFramesToFfMpegProcess()
+        {
+            var input = _ffmpeg.StandardInput.BaseStream;
 
             using (var writer = new BinaryWriter(input))
             {
-                var frameCount = (int)Math.Round(vidja.Fps * vidja.Duration);
+                var frameCount = (int) Math.Round(_vidja.Fps*_vidja.Duration);
 
-                var startFrame = startAt * vidja.Fps;
+                var startFrame = _startAt*_vidja.Fps;
 
                 for (var frameCounter = startFrame; frameCounter < frameCount; frameCounter++)
                 {
-                    var t = frameCounter / (double)vidja.Fps;
-                    var bitmap = vidja.RenderFrame(t);
-
-                    using (var ms = new MemoryStream())
-                    {
-                        bitmap.Save(ms, ImageFormat.Bmp);
-                        var buffer = ms.ToArray();
-
-                        writer.Write(buffer, 0, buffer.Length);
-                    }
+                    var t = frameCounter/(double) _vidja.Fps;
+                    WriteFrameToFfMpegProcess(t, writer);
                 }
                 writer.Close();
-                ffmpeg.WaitForExit();
+                _ffmpeg.WaitForExit();
+            }
+        }
+
+        private static void WriteFrameToFfMpegProcess(double t, BinaryWriter writer)
+        {
+            var bitmap = _vidja.RenderFrame(t);
+
+            using (var ms = new MemoryStream())
+            {
+                bitmap.Save(ms, ImageFormat.Bmp);
+                var buffer = ms.ToArray();
+
+                writer.Write(buffer, 0, buffer.Length);
             }
         }
     }
