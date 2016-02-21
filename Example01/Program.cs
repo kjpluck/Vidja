c6e2fb46eef0c7f6d296a60090acdc33905aee26
Kevin Pluck <kevinp@micad.co.uk>
2016-02-21 23:38:27 +0000

    Added example 1

---
+++ Example01/Program.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using Vidja;
+
+namespace Example01
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Vidja.Vidja.Generate(new Eg01(), "Eg01.gif");
+        }
+        
+    }
+
+    public class Eg01:IVidja
+    {
+        public int Width { get; } = 128;
+        public int Height { get; } = 128;
+        public int Fps { get; } = 15;
+        public double Duration { get; } = 2;
+
+        public Bitmap RenderFrame(double t)
+        {
+            var bmp = new Bitmap(Width, Height);
+            var graphics = Graphics.FromImage(bmp);
+            graphics.SmoothingMode = SmoothingMode.AntiAlias;
+
+            var radius = (float)(Width * (1 + Math.Pow((t * (Duration - t)), 2)) / 6);
+            graphics.FillCircle(Brushes.Red, 64, 64, radius);
+            return bmp;
+        }
+    }
+}
---
+++ Vidja/VidjaGraphicsExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Drawing;
+
+namespace Vidja
+{
+    public static class VidjaGraphicsExtensions
+    {
+        public static void FillCircle(this Graphics graphics, Brush brush, float x, float y, float radius)
+        {
+            var diameter = radius * 2;
+            graphics.FillEllipse(brush, x - radius, y - radius, diameter, diameter);
+        }
+    }
+}
