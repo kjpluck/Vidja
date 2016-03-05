108d30ff7cab03f69682fc6107afa4e5e80229d4
Kevin Pluck <kevinp@micad.co.uk>
2016-03-05 22:50:58 +0000

    Use FFMpeg's gif palette generator

--- Vidja/Vidja.cs
+++ Vidja/Vidja.cs
@@ -13,6 +13,7 @@ namespace Vidja
         private static string _ffMpegPath;
         private static int _startAt;
         private static string _outputFilename;
+        private static readonly string TempDir = Environment.GetEnvironmentVariable("TEMP");
 
         public static void Generate(IVidja vidja, string outputFilename = "vidja.mp4", int startAt = 0)
         {
@@ -22,9 +23,68 @@ namespace Vidja
 
             CheckForFfMpegExe();
 
-            StartFfMpegProcess();
+            if (OutputIsAGif(outputFilename))
+            {
+                // The default gif palette used by FFMpeg makes mediocre gifs
+                // but it can accept a custom palette which FFMpeg can also generate (which is nice)
+                // First we run all the frames through FFMpeg to generate this palette which is simply a png saved in the temp directory
+                ExtractPalette();
 
+                // Then create the gif using that palette.
+                // TODO cache the generated frames from above
+                CreateGifAnimation();
+
+                DeletePalette();
+            }
+            else
+            {
+                CreateAnimation();
+            }
+        }
+
+        private static bool OutputIsAGif(string outputFilename)
+        {
+            return outputFilename.ToLower().EndsWith(".gif");
+        }
+
+        private static void ExtractPalette()
+        {
+            StartFfMpegProcess($"-i pipe:0 -vf \"palettegen\" -y {TempDir}\\palette.png");
             PipeFramesToFfMpegProcess();
+        }
+
+        private static void CreateGifAnimation()
+        {
+            StartFfMpegProcess($"-i pipe:0 -i {TempDir}\\palette.png -lavfi \"fps={_vidja.Fps} [x]; [x][1:v] paletteuse\" -y {_outputFilename}");
+            PipeFramesToFfMpegProcess();
+        }
+
+        private static void CreateAnimation()
+        {
+            StartFfMpegProcess($"-y -video_size {_vidja.Width}x{_vidja.Height} -r {_vidja.Fps} -i pipe:0 -pix_fmt bgr24 {_outputFilename}");
+            PipeFramesToFfMpegProcess();
+        }
+
+        private static void DeletePalette()
+        {
+            File.Delete($"{TempDir}\\palette.png");
+        }
+
+        private static void StartFfMpegProcess(string arguments)
+        {
+            _ffmpeg = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = _ffMpegPath,
+                    Arguments = arguments,
+                    UseShellExecute = false,
+                    RedirectStandardInput = true,
+                    WindowStyle = ProcessWindowStyle.Hidden
+                }
+            };
+
+            _ffmpeg.Start();
         }
 
         private static void CheckForFfMpegExe()
@@ -34,26 +94,8 @@ namespace Vidja
             if (!File.Exists(_ffMpegPath))
             {
                 throw new InvalidOperationException(
-                    "Vidja uses FFmpeg to generate video output.  If you have it installed please set the path to ffmpeg.exe in App.config otherwise please download FFmpeg from http://ffmpeg.org/");
+                    "Don't be dismayed! Just one more step: Vidja uses FFmpeg to generate the animation output.  If you have it installed please set the path to ffmpeg.exe in App.config otherwise please download FFmpeg from http://ffmpeg.org/");
             }
-        }
-
-        private static void StartFfMpegProcess()
-        {
-            _ffmpeg = new Process
-            {
-                StartInfo = new ProcessStartInfo
-                {
-                    FileName = _ffMpegPath,
-                    Arguments =
-                        $"-y -video_size {_vidja.Width}x{_vidja.Height} -r {_vidja.Fps} -i pipe:0 -pix_fmt bgr24 {_outputFilename}",
-                    UseShellExecute = false,
-                    RedirectStandardInput = true,
-                    WindowStyle = ProcessWindowStyle.Hidden
-                }
-            };
-
-            _ffmpeg.Start();
         }
 
         private static void PipeFramesToFfMpegProcess()
